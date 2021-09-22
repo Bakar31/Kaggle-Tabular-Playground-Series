6bcf59789715ff6c6b47e6975123f9bf9c997b7f
Bakar31 <abubakar1808031@gmail.com>
2021-09-22 21:26:08 +0600

    input

---
+++ Jun 2021/src/input.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 27 16:21:30 2021
+
+@author: Abu Bakar
+"""
+
+import pandas as pd
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+sample_sub = pd.read_csv('sample_submission.csv')
